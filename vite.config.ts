b2825802818f5a873b71bcd96e87beed295991e2
Carolina Feitosa <carolfefe500@gmail.com>
2025-12-01 16:24:03 -0300

    Merge da branch gh-pages para main

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), "");
 
   return {
-    base: env.VITE_BASE_PATH || "/",
+    base: env.VITE_BASE_PATH || "/Amae/",
     server: {
       host: "::",
       port: 8080,
